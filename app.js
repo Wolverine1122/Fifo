b0af84ace7de319851e814a2ba011c56f8606356
Behzod <bmoydinboyev@usf.edu>
2021-08-14 16:42:12 +0500

    Add custom scroll bar and popup card

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require("express")
 const path = require("path")
+const https = require("https");
+const { response } = require("express");
+const { send } = require("process");
 
 const recommendedResultNumber = 9;
+const ingredientsListItemsNumber = 6;
 
 const app = express()
 
@@ -9,10 +13,11 @@ app.set("view engine", "ejs")
 app.set("views", path.join(__dirname, "views"))
 
 app.use(express.urlencoded({ extended: false }))
+app.use(express.json()) // To parse the incoming requests with JSON payloads
 app.use(express.static(path.join(__dirname, "public")))
 
 app.get("/", (req, res) => {
-    res.render("main")
+    res.render("main");
 })
 
 app.get("/search.html", (req, res) => {
@@ -21,8 +26,34 @@ app.get("/search.html", (req, res) => {
         foodType: "Desert",
         foodSummary: "Vegan Dirty Chai Pudding is a <b>gluten free and dairy free</b> recipe with 6 servings. One serving contains <b>325 calories</b>, <b>5g of protein</b>, and <b>22g of fat</b>. For <b>$1.93 per serving</b>, this recipe <b>covers 16%</b> of your daily requirements of vitamins and minerals. It works well as a dessert. 6 people have made this recipe and would make it again. This recipe from Foodista requires pudding, cinnamon, vanilla, and cocoa powder. From preparation to the plate, this recipe takes roughly <b>roughly 45 minutes</b>. Overall, this recipe earns a <b>pretty good spoonacular score of 76%</b>. Try <a href=\"https://spoonacular.com/recipes/dirty-chai-tapioca-pudding-with-oatmeal-raisin-cookie-crumble-940972\">Dirty Chai Tapioca Pudding with Oatmeal Raisin Cookie Crumble</a>, <a href=\"https://spoonacular.com/recipes/dirty-dirty-chai-625782\">Dirty Dirty Chai</a>, and <a href=\"https://spoonacular.com/recipes/dirty-chai-541786\">Dirty Chai</a> for similar recipes.",
         timeToCookFood: "15 minutes",
-        recommendedResultNumber
+        ingredientsListItemName: "Cake",
+        recommendedResultNumber,
+        ingredientsListItemsNumber
     })
+})
+
+app.post ("/search.html", (req, res) => {
+    const queryFood = req.body.searchFood;
+    const apiKey = "cd57b8f7f12743c780c85143ee791446";
+    const url = "https://api.spoonacular.com/food/products/search?query=" + queryFood + "&apiKey=" + apiKey;
+
+    https.get(url, function(response) {
+        if (response.statusCode == 404) {
+            res.sendStatus(400);
+            return true;
+        }
+
+        let result = '';
+        response.on("data", (data) => {
+            result += data;
+        })
+        response.on("end", () => {
+            const foodRecipeData = JSON.parse(result);
+            const products = foodRecipeData.products;
+            console.log(products.length);
+            res.json(foodRecipeData);
+        })
+    })  
 })
 
 app.listen(3000)
--- public/js/search.js
+++ public/js/search.js
@@ -1,13 +1,50 @@
+let truncateCounter = 0;
+
+//Show the value of slider in text
 function updateSliderValue(sliderValue, sliderValueText) {
-  document.getElementById(sliderValueText).innerHTML = document.getElementById(sliderValue).value;
+  $("#" + sliderValueText).html($("#" + sliderValue).val());
 }
 
+//Display and hide the popup card
 function displayFoodCard() {
-  document.querySelector(".popup-box").classList.add("popup-box-appear");
-  document.querySelector(".darken-background").style.display="block";
+  $(".popup-box").addClass("popup-box-appear");
+  $(".darken-background").css("display", "block");
+}
+function hideFoodCard() {
+  $(".popup-box").removeClass("popup-box-appear");
+  $(".darken-background").css("display", "none");
 }
 
-function hideFoodCard() {
-  document.querySelector(".popup-box").classList.remove("popup-box-appear");
-  document.querySelector(".darken-background").style.display="none";
+// Slice text in food summary on card
+function truncateParagraph (targetParagraph) {
+  if (truncateCounter == 0) {
+    $("." + targetParagraph).addClass("truncate");
+    $(".toggleTruncateClick").html("Read More");
+    truncateCounter++;
+  } else {
+    $("." + targetParagraph).removeClass("truncate");
+    $(".toggleTruncateClick").html("Read Less");
+    truncateCounter = 0;
+  }
 }
+truncateParagraph('foodSummary');
+
+
+
+//Change card display to/from full
+function changeCardDisplayDimension() {
+  $(".popup-box").toggleClass("cardDisplayFull");
+}
+
+
+// 1. Simple mode, it styles document scrollbar:
+$(function() {  
+  $("body").niceScroll({
+    cursorcolor: "hsl(31, 94%, 55%)",
+    cursorwidth:"12px"
+  });
+  $(".popup-box").niceScroll({
+    cursorcolor: "hsl(31, 94%, 55%)",
+    cursorwidth:"10px"
+  })
+});
